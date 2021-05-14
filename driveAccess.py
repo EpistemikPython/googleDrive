eae7b5ed18992af5aeba1413d48db7b191d7d9a8
epistemik <epistemik@gmail.com>
2021-05-14 15:05:04 -0400

    metadata read and file upload are working

--- driveAccess.py
+++ driveAccess.py
@@ -1,105 +1,106 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# googleUtils.py -- useful constants, functions & classes for access to Google entities
+# driveAccess.py -- useful constants, functions & classes for access to my Google Drive
 #
 # includes some code from Google quickstart examples
 #
-# Copyright (c) 2019-21 Mark Sattolo <epistemik@gmail.com>
+# Copyright (c) 2021 Mark Sattolo <epistemik@gmail.com>
 
 __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __google_api_python_client_py3_version__ = "1.2"
-__created__ = "2019-04-07"
-__updated__ = "2021-05-11"
+__created__ = "2021-05-14"
+__updated__ = "2021-05-14"
 
+import sys
 import threading
-from decimal import Decimal
-from sys import path
-import pickle5 as pickle
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from typing import Union
-path.append("/newdata/dev/git/Python/Gnucash/common")
-from investment import *
+from googleapiclient.http import MediaFileUpload
+sys.path.append("/newdata/dev/git/Python/utils")
+from mhsUtils import get_base_filename, get_current_time, osp, lg
 
 # see https://github.com/googleapis/google-api-python-client/issues/299
 # use: e.g. build("drive", "v3", http=http, cache_discovery=False)
 lg.getLogger("googleapiclient.discovery_cache").setLevel(lg.ERROR)
 
-BASE_ROW:str = "Base Row"
-# sheet names in Budget Quarterly
-ML_WORK_SHEET:str  = "ML Work"
-CALCULNS_SHEET:str = "Calculations"
-
-SECRETS_DIR = "/newdata/dev/git/Python/Google/secrets"
+SECRETS_DIR = "/newdata/dev/git/Python/Google/Drive/secrets"
 CREDENTIALS_FILE:str = osp.join(SECRETS_DIR, "credentials" + osp.extsep + "json")
-
-SHEETS_RW_SCOPE:list     = ['https://www.googleapis.com/auth/spreadsheets']
-BASE_SHEETS_PICKLE:str   = "token.sheets.epistemik.rw.pickle"
-BASE_PICKLE_LOCATION:str = osp.join(SECRETS_DIR, BASE_SHEETS_PICKLE)
-SHEETS_EPISTEMIK_RW_TOKEN:dict = {
-    "P2" : BASE_PICKLE_LOCATION + '2' ,
-    "P3" : BASE_PICKLE_LOCATION + '3' ,
-    "P4" : BASE_PICKLE_LOCATION + '4' ,
-    "P5" : BASE_PICKLE_LOCATION + '5'
-}
-GGL_SHEETS_TOKEN:str = SHEETS_EPISTEMIK_RW_TOKEN["P5"]
-
-# Spreadsheet ID
-BUDGET_QTRLY_ID_FILE:str = osp.join(SECRETS_DIR, "Budget-qtrly" + osp.extsep + "id")
-
-# sheet names in Budget Quarterly
-ALL_INC_SHEET:str    = "All Inc 1"
-ALL_INC_2_SHEET:str  = "All Inc 2"
-NEC_INC_SHEET:str    = "Nec Inc 1"
-NEC_INC_2_SHEET:str  = "Nec Inc 2"
-QTR_ASTS_SHEET:str   = "Assets 1"
-QTR_ASTS_2_SHEET:str = "Assets 2"
-BAL_1_SHEET:str      = "Balance 1"
-BAL_2_SHEET:str      = "Balance 2"
-
-FILL_CELL_VAL = Union[str, Decimal]
+DRIVE_ACCESS_SCOPE:list  = ["https://www.googleapis.com/auth/drive"]
+DRIVE_JSON_TOKEN:str     = "token.json"
+DRIVE_TOKEN_LOCATION:str = osp.join(SECRETS_DIR, DRIVE_JSON_TOKEN)
 
 
-# noinspection PyUnresolvedReferences
-def get_credentials(logger:lg.Logger=None) -> pickle:
+# google quickstart
+def test_metadata_read():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = get_credentials()
+    service = build("drive", "v3", credentials=creds)
+
+    # call the Drive v3 API
+    results = service.files().list( pageSize = 10,
+                                    fields = "nextPageToken, files(id, name)" ).execute()
+    items = results.get("files", [])
+
+    if not items:
+        print("No files found?!")
+    else:
+        print("Files:")
+        for item in items:
+            print(F"{item['name']} ({item['id']})")
+
+
+def test_upload(filepath:str):
+    creds = get_credentials()
+    service = build("drive", "v3", credentials=creds)
+
+    file_metadata = {"name":get_base_filename(filepath)}
+    media = MediaFileUpload(filepath, mimetype = "text/plain", resumable = True)
+
+    file = service.files().create( body = file_metadata,
+                                   # uploadType = multipart,
+                                   media_body = media,
+                                   fields = "id").execute()
+
+    print(F"File ID: {file.get('id')}")
+
+
+def get_credentials():
     """Get the proper credentials needed to write to the Google spreadsheet."""
     creds = None
-    if osp.exists(GGL_SHEETS_TOKEN):
-        if logger: logger.info(F"osp.exists({GGL_SHEETS_TOKEN})")
-        with open(GGL_SHEETS_TOKEN, "rb") as token:
-            creds = pickle.load(token)
-
-    # if there are no (valid) credentials available, let the user log in.
-    if creds is None or not creds.valid:
-        if logger: logger.info("creds is None or not creds.valid")
+    # The TOKEN file stores the user's access and refresh tokens & is
+    # created automatically when the authorization flow completes for the first time
+    if osp.exists(DRIVE_TOKEN_LOCATION):
+        creds = Credentials.from_authorized_user_file( DRIVE_TOKEN_LOCATION, DRIVE_ACCESS_SCOPE )
+    # if there are no (valid) credentials available, let the user log in
+    if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-            if logger: logger.debug("creds.refresh(Request())")
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SHEETS_RW_SCOPE)
-            creds = flow.run_local_server()
-            if logger: logger.debug("creds = flow.run_local_server()")
+            flow = InstalledAppFlow.from_client_secrets_file( CREDENTIALS_FILE, DRIVE_ACCESS_SCOPE )
+            creds = flow.run_local_server(port=0)
         # save the credentials for the next run
-        with open(GGL_SHEETS_TOKEN, "wb") as token:
-            if logger: logger.debug("pickle.dump()")
-            pickle.dump(creds, token, pickle.HIGHEST_PROTOCOL)
+        with open("token.json", 'w') as token:
+            token.write( creds.to_json() )
 
     return creds
 
 
-class GoogleUpdate:
-    """Start a Google session, read/write to my Budget sheet, end the session."""
+class MhsDriveAccess:
+    """Start a Google session, read/write to my google drive, end the session."""
     # prevent different instances/threads from writing at the same time
     _lock = threading.Lock()
 
-    def __init__(self, p_logger:lg.Logger):
+    def __init__(self, p_logger:lg.Logger=None):
         self._lgr = p_logger
         self._data = list()
-        self._lgr.info(F"\n\tLaunch {self.__class__.__name__} instance with lock {str(self._lock)}\n\t"
-                       F" at Runtime = {get_current_time()}\n")
+        if self._lgr:
+            self._lgr.info(F"Launch {self.__class__.__name__} instance with lock = {str(self._lock)} at {get_current_time()}")
 
     def get_data(self) -> list:
         return self._data
@@ -108,45 +109,32 @@ class GoogleUpdate:
     def begin_session(self):
         # PREVENT starting a separate Session on the Google file
         self._lock.acquire()
-        self._lgr.info(F"acquired lock at {get_current_time()}")
-        creds = get_credentials(self._lgr)
-        service = build("sheets", "v4", credentials = creds, cache_discovery = False)
-        self.vals = service.spreadsheets().values()
+        if self._lgr: self._lgr.info(F"acquired lock at {get_current_time()}")
+        creds = get_credentials()
+        service = build("drive", "v3", credentials = creds)
+        # service = build("sheets", "v4", credentials = creds, cache_discovery = False)
+        self.files = service.files()
 
     def end_session(self):
         # RELEASE the Session on the Google file
         self._lock.release()
-        self._lgr.debug(F"released lock at {get_current_time()}")
+        if self._lgr: self._lgr.debug(F"released lock at {get_current_time()}")
 
-    def __get_budget_id(self) -> str:
-        """Get the budget id string from the file in the secrets folder."""
-        with open(BUDGET_QTRLY_ID_FILE, "r") as bfp:
+    def __get_file_id(self, fn:str) -> str:
+        """Get the file id string from the file in the secrets folder."""
+        with open(fn, "r") as bfp:
             fid = bfp.readline().strip()
-        self._lgr.debug(F"{get_current_time()} / Budget Id = {fid}\n")
+        if self._lgr: self._lgr.debug(F"{get_current_time()} / File Id = {fid}\n")
         return fid
 
-    def fill_cell(self, sheet:str, col:str, row:int, val:FILL_CELL_VAL):
-        """
-        CREATE the information to update a Google Sheets cell and add to the data list
-        :param sheet: particular sheet in my Google spreadsheet to update
-        :param   col: column to update
-        :param   row: to update
-        :param   val: str OR Decimal: value to fill with
-        """
-        self._lgr.debug( get_current_time() )
-        value = val.to_eng_string() if isinstance(val, Decimal) else val
-        cell = {"range": sheet + '!' + col + str(row), "values": [[value]]}
-        self._lgr.debug(F"fill_cell() = {cell}\n")
-        self._data.append(cell)
-
     # noinspection PyTypeChecker
-    def send_sheets_data(self) -> dict:
+    def send_drive_data(self, files:list) -> dict:
         """
         SEND the data list to my Google sheets document
         :return: server response
         """
-        self._lgr.debug( get_current_time() )
-        if not self.vals:
+        if self._lgr: self._lgr.debug( get_current_time() )
+        if not self.files and self._lgr:
             self._lgr.exception("No Session started!")
 
         response = {"Response": "None"}
@@ -155,38 +143,58 @@ class GoogleUpdate:
                 "valueInputOption": "USER_ENTERED",
                 "data": self._data
             }
-            response = self.vals.batchUpdate(spreadsheetId=self.__get_budget_id(), body=assets_body).execute()
+            response = self.files.batchUpdate(spreadsheetId=self.__get_file_id(files[0]), body=assets_body).execute()
 
-            self._lgr.info(F"{response.get('totalUpdatedCells')} cells updated!\n")
+            if self._lgr: self._lgr.info(F"{response.get('totalUpdatedCells')} cells updated!\n")
         except Exception as ssde:
             msg = repr(ssde)
-            self._lgr.error(msg)
+            if self._lgr: self._lgr.error(msg)
             response["Response"] = msg
         return response
 
     # noinspection PyTypeChecker
-    def read_sheets_data(self, range_name:str) -> list:
+    def read_drive_data(self, range_name:str) -> list:
         """
         READ data from my Google sheets document
         :return: server response
         """
-        self._lgr.debug( get_current_time() )
-        if not self.vals:
+        if self._lgr: self._lgr.debug( get_current_time() )
+        if not self.files and self._lgr:
             self._lgr.exception("No Session started!")
         try:
-            response = self.vals.get(spreadsheetId = self.__get_budget_id(), range = range_name).execute()
+            response = self.files.get(spreadsheetId = self.__get_file_id(range_name), range = range_name).execute()
             rows = response.get("values", [])
-            self._lgr.info(F"{len(rows)} rows retrieved.\n")
+            if self._lgr: self._lgr.info(F"{len(rows)} rows retrieved.\n")
         except Exception as rsde:
             msg = repr(rsde)
-            self._lgr.error(msg)
+            if self._lgr: self._lgr.error(msg)
             rows = [msg]
         return rows
 
-    def test_read(self, range_name:str) -> list:
+    def test_send(self, file_name:str) -> dict:
         self.begin_session()
-        result = self.read_sheets_data(range_name)
+        result = self.send_drive_data([file_name])
         self.end_session()
         return result
 
-# END class GoogleUpdate
+# END class MhsDriveAccess
+
+
+def main_drive_access(p_filepath:str):
+    mhs = MhsDriveAccess()
+    response = mhs.test_send(p_filepath)
+    print( repr(response) )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        test_file = sys.argv[1]
+    else:
+        test_file = "0"
+    if osp.exists(test_file):
+        print(F"test file upload with '{test_file}'")
+        test_upload(test_file)
+    else:
+        print("test metadata read:")
+        test_metadata_read()
+    exit()

2b0e3fea218466d64b7df70c2a6d70c89443a0eb
epistemik <epistemik@gmail.com>
2021-07-06 11:08:19 -0400

    use ArgumentParser; properly set and handle program parameters

--- driveAccess.py
+++ driveAccess.py
@@ -11,26 +11,28 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __google_api_python_client_py3_version__ = "1.2"
 __created__ = "2021-05-14"
-__updated__ = "2021-07-05"
+__updated__ = "2021-07-06"
 
 import os
 import shutil
 import sys
 import threading
+from argparse import ArgumentParser
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 sys.path.append("/newdata/dev/git/Python/utils")
-from mhsLogging import get_simple_logger
-from mhsUtils import get_base_filename, get_current_time, osp, lg, file_ts, BASE_PYTHON_FOLDER
+from mhsLogging import get_simple_logger, MhsLogger, DEFAULT_LOG_LEVEL
+from mhsUtils import get_base_filename, get_filename, get_current_time, osp, lg, file_ts, BASE_PYTHON_FOLDER
 SECRETS_DIR:str = F"{BASE_PYTHON_FOLDER}/Google/Drive/secrets"
 sys.path.append(SECRETS_DIR)
 from folder_ids import FOLDER_IDS
 
+base_run_file = get_base_filename(__file__)
+
 # see https://github.com/googleapis/google-api-python-client/issues/299
-# use: e.g. build("drive", "v3", http=http, cache_discovery=False)
 lg.getLogger("googleapiclient.discovery_cache").setLevel(lg.ERROR)
 
 JSON_TOKEN = "token.json"
@@ -38,6 +40,7 @@ CREDENTIALS_FILE:str    = osp.join(SECRETS_DIR, "credentials.json")
 DRIVE_TOKEN_PATH:str    = osp.join(SECRETS_DIR, JSON_TOKEN)
 DRIVE_ACCESS_SCOPE:list = ["https://www.googleapis.com/auth/drive"]
 
+DEFAULT_NUM_FILES = 25
 MIMETYPE_TEXT          = "text/plain"
 MIMETYPE_GOOGLE_FOLDER = "application/vnd.google-apps.folder"
 MIMETYPE_GNUCASH       = "application/x-gnucash"
@@ -91,7 +94,7 @@ class MhsDriveAccess:
         self._lock.release()
         self._lgr.debug(F"released Drive lock at {get_current_time()}")
 
-    def send_file(self, filepath:str, p_parent:str=None) -> str:
+    def send_file(self, filepath:str, parent_id:str) -> str:
         """SEND a file to my Google drive
         :return server response
         """
@@ -100,22 +103,20 @@ class MhsDriveAccess:
             self._lgr.exception("No Session started!")
             return ""
         try:
-            file_metadata = {"name":get_base_filename(filepath)}
-            if p_parent:
-                file_metadata["parents"] = [p_parent]
+            file_metadata = {"name":get_filename(filepath), "parents":[parent_id]}
             media = MediaFileUpload(filepath, mimetype = MIMETYPE_TEXT, resumable = True)
-            self._lgr.info(F"Send file '{filepath}' to Drive:/{p_parent if p_parent else 'root'}")
+            self._lgr.info(F"Send file '{filepath}' to Drive://{parent_id}/")
 
             file = self.fserv.create(body = file_metadata, media_body = media, fields = "id").execute()
             response = file.get("id")
-            self._lgr.info(F"File ID: {response}")
+            self._lgr.info(F"Sent file: Id = {response}")
         except Exception as sfe:
             response = repr(sfe)
             self._lgr.error(response)
 
         return response
 
-    def read_file_info(self, p_mimetype:str = MIMETYPE_TEXT, p_numitems:int = 25):
+    def read_file_info(self, p_mimetype:str = MIMETYPE_TEXT, p_numitems:int = DEFAULT_NUM_FILES):
         """READ file data from my Google drive."""
         self._lgr.debug( get_current_time() )
         if not self.fserv:
@@ -130,10 +131,10 @@ class MhsDriveAccess:
             if not items:
                 self._lgr.error("No files found?!")
             else:
-                self._lgr.info("Files:")
+                self._lgr.info(F"{len(items)} files retrieved:")
+                self._lgr.info(" Name\t\t<type>\t\t(Id)\t\t\t[parent id]")
                 for item in items:
-                    self._lgr.info(F"{item['name']} <{item['mimeType']}> ({item['id']}) {item['parents']}")
-            self._lgr.info(F"{len(items)} files retrieved.\n")
+                    self._lgr.info(F"{item['name']} <{item['mimeType']}> ({item['id']}) {item['parents'] if item['parents'] else 'None'}")
         except Exception as rde:
             self._lgr.error(repr(rde))
 
@@ -145,6 +146,8 @@ class MhsDriveAccess:
             return
         try:
             page_token = None
+            self._lgr.info("Folders:")
+            self._lgr.info(" Name\t\t(Id)\t\t\t[parent id]")
             while True:
                 response = self.fserv.list( q = F"mimeType='{MIMETYPE_GOOGLE_FOLDER}'",
                                             spaces = "drive",
@@ -161,38 +164,68 @@ class MhsDriveAccess:
 # END class MhsDriveAccess
 
 
+def process_args():
+    arg_parser = ArgumentParser(description="Send to or request information from my Google Drive",
+                                prog="driveAccess.py")
+    # optional arguments
+    arg_parser.add_argument('--folders',  action = "store_true", help = "Get information on all my Google drive folders")
+    arg_parser.add_argument('-s', '--send', help = "path & name of the file to send")
+    arg_parser.add_argument('-p', '--parent', default = "root", help = "name of the Drive parent folder to send to")
+    arg_parser.add_argument('-m', '--mimetype', default = MIMETYPE_TEXT, help = "mimetype of files to gather info on")
+    arg_parser.add_argument('-n', '--numfiles', type = int, default = DEFAULT_NUM_FILES,
+                            help = "number of files to gather info on")
+    return arg_parser
+
+
+def process_input_parameters(argx:list):
+    args = process_args().parse_args(argx)
+    info = [F"args = {args}"]
+
+    if args.send and not osp.isfile(args.send):
+        raise Exception(F"File path '{args.send}' does not exist! Exiting...")
+    info.append(F"Send file = {args.send}")
+
+    parent = args.parent
+    if parent not in FOLDER_IDS.keys():
+        raise Exception(F"Parent folder '{parent}' does not exist! Exiting...")
+    parent_id = FOLDER_IDS[parent]
+
+    return args.folders, args.send, parent, parent_id, args.mimetype, args.numfiles
+
+
+def main_drive(argl:list):
+    folders, sendfile, parent, parent_id, mimetype, numfiles = process_input_parameters(argl)
+
+    log_control = MhsLogger(base_run_file, con_level = DEFAULT_LOG_LEVEL)
+    log_control.show(F"Runtime = {get_current_time()}")
+    lgr = log_control.get_logger()
+    lgr.debug( repr(lgr.handlers) )
+
+    mhs = MhsDriveAccess(lgr)
+    try:
+        mhs.begin_session()
+        if folders:
+            print("test finding folders:")
+            mhs.find_all_folders()
+
+        elif sendfile and parent_id:
+            print(F"test upload of file: {sendfile} to Drive folder: {parent if parent else 'root'}")
+            mhs.send_file(sendfile, parent_id)
+
+        elif mimetype and numfiles:
+            print(F"test reading info from {numfiles} {mimetype} files:")
+            mhs.read_file_info(p_mimetype = mimetype, p_numitems = numfiles)
+
+        else:
+            print("NO parameters.")
+
+    except Exception as de:
+        print( repr(de) )
+    finally:
+        if mhs:
+            mhs.end_session()
+
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        mhs = None
-        try:
-            test_parameter = sys.argv[1]
-            mhs = MhsDriveAccess()
-            mhs.begin_session()
-            if osp.exists(test_parameter):
-                # test file send
-                parent = parent_id = None
-                if len(sys.argv) > 2:
-                    parent = sys.argv[2]
-                    if parent in FOLDER_IDS.keys():
-                        parent_id = FOLDER_IDS[parent]
-                print(F"test upload of file: {test_parameter} to Drive folder: {parent if parent else 'root'}")
-                mhs.send_file(test_parameter, parent_id)
-            elif test_parameter == "folders":
-                print("test finding folders:")
-                mhs.find_all_folders()
-            else:
-                # test reading file info for a particular mimetype
-                mtype = MIMETYPE_TEXT
-                num_files = int(test_parameter)
-                if len(sys.argv) > 2:
-                    mtype = sys.argv[2]
-                print(F"test reading info from {test_parameter} {mtype} files:")
-                mhs.read_file_info(p_mimetype = mtype, p_numitems = num_files)
-        except Exception as me:
-            print( repr(me) )
-        finally:
-            if mhs:
-                mhs.end_session()
-    else:
-        print("NO parameters.")
+    main_drive(sys.argv[1:])
     exit()

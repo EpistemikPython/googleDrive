4d912fe822e32ff721fe6b81e0f0377cbc881a10
epistemik <epistemik@gmail.com>
2024-09-21 12:22:41 -0400

    use re to get a simpler and more comprehensive checking of the date arg; updates to logging messages; &

--- driveCleanup.py
+++ driveCleanup.py
@@ -14,11 +14,12 @@ __python_version__ = "3.6+"
 __google_api_python_client_version__ = "2.144.0"
 __google_auth_oauthlib_version__     = "1.2.1"
 __created__ = "2024-09-08"
-__updated__ = "2024-09-12"
+__updated__ = "2024-09-19"
 
 from driveAccess import *
+import re
 
-DEFAULT_DATE = "2027-09-13"
+DEFAULT_DATE = "2027-11-13"
 DEFAULT_FILETYPE = "gcm"
 DEFAULT_PARENT_FOLDER = "Test"
 MAX_FILES_DELETE = 500
@@ -26,11 +27,10 @@ MAX_FILES_DELETE = 500
 # see https://github.com/googleapis/google-api-python-client/issues/299
 lg.getLogger("googleapiclient.discovery_cache").setLevel(lg.ERROR)
 
-
 def get_files():
     """retrieve files in the specified parent folder that are older than the specified date"""
     query = f"modifiedTime < '{fdate}' and '{parent_id}' in parents"
-    lgr.info(f"query = '{query}'")
+    lgr.info(f"query: {query}")
     results = mhsda.service.list(q = query, spaces = "drive", pageSize = MAX_FILES_DELETE,
                                  fields = "files(name, id, parents, mimeType, modifiedTime)").execute()
     items = results.get("files", [])
@@ -46,17 +46,18 @@ def get_files():
 
     return items
 
-def delete_file(p_name, p_file_id):
+def delete_file(p_name:str, p_file_id:str, p_filedate:str):
     """Delete a file.
     :arg    p_name: name of the file
     :arg    p_file_id: ID of the file to delete
+    :arg    p_filedate: modified time of the file
     """
     if testing_mode:
-        response = f"Testing: Would have deleted file '{p_name}' with id: {p_file_id}"
+        response = f"Testing: Would have deleted file '{p_name}' with date: {p_filedate}"
         lgr.info(response)
     else:
-        response = mhsda.service.delete(fileId = 'file_id').execute()
-        lgr.info(f"response[{p_file_id}] = '{response}'.")
+        response = mhsda.service.delete(fileId = p_file_id).execute()
+        lgr.info(f"response[{p_name} @ {p_filedate}] = '{response}'.")
 
     return response
 
@@ -67,11 +68,11 @@ def set_args():
     arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the response to a JSON file")
     arg_parser.add_argument('-t', '--test', action="store_true", default=False, help="Testing mode; DEFAULT = False")
     arg_parser.add_argument('-f', '--filetype', type=str, default=f"{DEFAULT_FILETYPE}",
-                            help = f"type of file to delete; DEFAULT = '{DEFAULT_FILETYPE}'")
+                            help = f"filename extension of files to delete; DEFAULT = '{DEFAULT_FILETYPE}'")
     arg_parser.add_argument('-d', '--date', type=str, default=DEFAULT_DATE,
                             help = f"delete ALL files BEFORE this date [YYYY-MM-DD]; DEFAULT = '{DEFAULT_DATE}'")
     arg_parser.add_argument('-p', '--parent', type=str, default=f"{DEFAULT_PARENT_FOLDER}",
-                            help = f"folder containing the files to delete; DEFAULT = '{DEFAULT_PARENT_FOLDER}'")
+                            help = f"Drive folder containing the files to delete; DEFAULT = '{DEFAULT_PARENT_FOLDER}'")
     return arg_parser
 
 def get_args(argl:list):
@@ -86,7 +87,10 @@ def get_args(argl:list):
     lgr.info(f"DELETING files in folder {args.parent}; id = {parid}")
 
     adt = args.date
-    if not adt[:4].isnumeric() and adt[4:6].isnumeric() and adt[6:8].isnumeric():
+    # will reject most improper dates
+    dtre = re.compile("[1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]")
+    dtmatch = re.match(dtre, adt)
+    if not dtmatch:
         raise Exception(f"Date '{adt}' in IMPROPER format.")
     ts = f"{adt}T01:02:03"
     lgr.info(f"DELETING files OLDER than: {ts}\n")
@@ -102,13 +106,14 @@ def run():
             fname = item["name"]
             ftype = get_filetype(fname)[1:]
             if ftype == filetype:
-                result = delete_file(fname, item["id"])
-                deletes.append(result)
+                result = delete_file(fname, item["id"], item["modifiedTime"])
+                if result:
+                    deletes.append(result)
         if save_option and deletes:
             jfile = save_to_json(get_base_filename(argv[0]), deletes)
             lgr.info(f"Saved results to '{jfile}'.")
-    except Exception as mdex:
-        return mdex
+    except Exception as rex:
+        raise rex
     finally:
         if mhsda:
             mhsda.end_session()
@@ -128,11 +133,11 @@ if __name__ == "__main__":
         lgr.exception(">> User interruption.")
         code = 13
     except ValueError:
-        lgr.error(">> Value error.")
-        code = 39
+        lgr.exception(">> Value error.")
+        code = 27
     except HttpError:
-        lgr.error(">> Http error.")
-        code = 53
+        lgr.exception(">> googleapiclient Http error.")
+        code = 39
     except Exception as mex:
         lgr.exception(f"Problem: {repr(mex)}.")
         code = 66

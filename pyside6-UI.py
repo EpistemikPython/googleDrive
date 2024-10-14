8548c97efddc3abfe7cf3a1910a20589a7d7f026
epistemik <epistemik@gmail.com>
2024-10-14 13:09:55 -0400

    more progress on implementing function and options selections

--- pyside6-UI.py
+++ pyside6-UI.py
@@ -23,6 +23,7 @@ from driveFunctions import *
 
 FILE_LABEL:str   = "File"
 FOLDER_LABEL:str = "Folder"
+NUMFILES_LABEL:str = "Choose the number of files"
 SEND_LABEL:str   = " to send:"
 LOG_LABEL:str    = "Change the logging level?"
 ROOT_LABEL:str   = "root"
@@ -40,8 +41,6 @@ class DriveFunctionsUI(QDialog):
         self.top = 64
         self.width = 660
         self.height = 800
-        self.mon_file = None
-        self.gnc_file = None
         self._lgr = log_control.get_logger()
 
         self.init_ui()
@@ -74,33 +73,35 @@ class DriveFunctionsUI(QDialog):
         self.gb_main = QGroupBox("Parameters")
         layout = QFormLayout()
 
+        self.selected_function = FUNCTIONS[0]
         self.cb_fxn = QComboBox()
         self.cb_fxn.addItems(FUNCTIONS)
         self.cb_fxn.currentIndexChanged.connect(self.fxn_change)
         layout.addRow(QLabel("Function to run:"), self.cb_fxn)
 
-        self.add_file_btn()
-        layout.addRow(self.file_label, self.file_btn)
+        self.fsend_title = "Get local file or folder"
+        self.filesend_title = "Get local file"
+        self.foldersend_title = "Get local folder"
+        self.pb_fsend = QPushButton(self.fsend_title)
+        self.fsend_label = QLabel("Send:")
+        self.pb_fsend.clicked.connect(partial(self.open_file_name_dialog, self.fsend_title))
+        layout.addRow(self.fsend_label, self.pb_fsend)
 
-        self.add_folder_btn()
-        layout.addRow(self.folder_label, self.folder_btn)
-
-        self.pb_drive_folder = QPushButton("Select the Drive folder to use.")
+        self.do_title = "Choose Drive folder/file"
+        self.do_file_title = "Choose Drive file"
+        self.do_folder_title = "Choose Drive folder"
+        self.pb_drive_option = QPushButton(self.do_title)
         self.drive_folder = ROOT_LABEL
-        self.pb_drive_folder.clicked.connect(self.get_drive_folder)
-        layout.addRow(QLabel("Drive folder:"), self.pb_drive_folder)
-
-        self.pb_meta_filename = QPushButton("Select the Drive file to query for metadata.")
         self.meta_filename = DEFAULT_METADATA_FILE
-        self.pb_meta_filename.clicked.connect(self.get_meta_filename)
-        layout.addRow(QLabel("Metadata:"), self.pb_meta_filename)
+        self.pb_drive_option.clicked.connect(self.get_drive_option)
+        layout.addRow(QLabel("Drive option:"), self.pb_drive_option)
 
-        self.cb_mode = QComboBox()
-        self.cb_mode.addItems([TEST_FOLDER, 'PRICE', 'TRADE', 'BOTH'])
-        self.cb_mode.currentIndexChanged.connect(self.mode_change)
-        layout.addRow(QLabel('Mode:'), self.cb_mode)
+        self.num_files = 1
+        self.pb_numfiles = QPushButton(NUMFILES_LABEL)
+        self.pb_numfiles.clicked.connect(self.get_log_level)
+        layout.addRow(QLabel("Number of files:"), self.pb_numfiles)
 
-        self.chbx_json = QCheckBox("Save Monarch info to JSON file?")
+        self.chbx_json = QCheckBox("Save function info to JSON file?")
         layout.addRow(QLabel("Save:"), self.chbx_json)
 
         self.pb_logging = QPushButton(LOG_LABEL)
@@ -108,120 +109,105 @@ class DriveFunctionsUI(QDialog):
         layout.addRow(QLabel("Logging:"), self.pb_logging)
 
         self.exe_btn = QPushButton("Go!")
-        self.exe_btn.setStyleSheet("QPushButton {font-weight: bold; color: red; background-color: yellow;}")
+        self.exe_btn.setStyleSheet("QPushButton {font-weight: bold; color: yellow; background-color: red;}")
         self.exe_btn.clicked.connect(self.button_click)
         layout.addRow(QLabel("EXECUTE:"), self.exe_btn)
 
         self.gb_main.setLayout(layout)
 
-    def add_file_btn(self):
-        self.file_btn_title = f"Get local file"
-        self.file_btn = QPushButton(self.file_btn_title)
-        self.file_label = QLabel(FILE_LABEL + SEND_LABEL)
-        self.file_btn.clicked.connect(partial(self.open_file_name_dialog, FILE_LABEL))
-
-    def add_folder_btn(self):
-        self.folder_btn_title = f"Get local folder"
-        self.folder_btn = QPushButton(self.folder_btn_title)
-        self.folder_label = QLabel(FOLDER_LABEL + SEND_LABEL)
-        self.folder_btn.clicked.connect(partial(self.open_file_name_dialog, FOLDER_LABEL))
+    def fxn_change(self):
+        sf = self.cb_fxn.currentText()
+        basic_stylesheet = self.cb_fxn.styleSheet()
+        self._lgr.info(f"basic stylesheet = '{basic_stylesheet}'")
+        self._lgr.info(f"selected function changed to '{sf}'")
+        if sf == FUNCTIONS[3] or sf == FUNCTIONS[2]: # send file or folder | option: drive folder to send to
+            s_title = self.filesend_title if sf == FUNCTIONS[3] else self.foldersend_title
+            self.pb_fsend.setText(s_title)
+            self.pb_fsend.setStyleSheet("QPushButton {font-weight: bold; color: red; background-color: cyan;}")
+            self.pb_drive_option.setText(self.do_folder_title)
+            self.pb_drive_option.setStyleSheet("QPushButton {font-weight: bold; background-color: white;}")
+            self.pb_numfiles.setText(NO_NEED)
+            self.pb_numfiles.setStyleSheet("")
+        # elif sf == FUNCTIONS[2]: # send folder | option: drive folder to send to
+        #     self.pb_fsend.setText(self.foldersend_title)
+        #     self.pb_fsend.setStyleSheet("QPushButton {font-weight: bold; color: red; background-color: cyan;}")
+        #     self.pb_drive_option.setText(self.do_folder_title)
+        #     self.pb_drive_option.setStyleSheet("QPushButton {font-weight: bold; background-color: white;}")
+        #     self.pb_numfiles.setText(NO_NEED)
+        #     self.pb_numfiles.setStyleSheet("")
+        elif sf == FUNCTIONS[4]: # metadata | option: name of file to query
+            self.pb_drive_option.setText(self.do_file_title)
+            self.pb_drive_option.setStyleSheet("QPushButton {font-weight: bold; color: red; background-color: cyan;}")
+            self.pb_fsend.setText(NO_NEED)
+            self.pb_fsend.setStyleSheet("")
+            self.pb_numfiles.setText(NO_NEED)
+            self.pb_numfiles.setStyleSheet("")
+        elif sf == FUNCTIONS[5]: # delete
+            pass
+            # option: drive folder with files to delete; default = TEST_FOLDER
+            # option: type of files to delete; default = DEFAULT_FILETYPE
+            # option: date to delete files OLDER THAN; default = DEFAULT_DATE
+            # option: TEST mode: NO deletions, just report; default = False
+        elif sf == FUNCTIONS[1]: # get files
+            pass
+            # ?? ADD? option: drive folder with files to retrieve info; default = root
+            # option: type of files to retrieve info; default = DEFAULT_FILETYPE
+            # option: type of files is a MimeType instead of a filename extension; default = False
+            # option: NUMBER of files to retrieve info on; default = DEFAULT_NUM_FILES, max = MAX_NUM_FILES
+        elif sf == FUNCTIONS[0]: # get all folders
+            pass
+            # None
+        else:
+            raise Exception("?? INVALID Function Choice??!!")
+        self.selected_function = sf
 
     def open_file_name_dialog(self, label:str):
-        self._lgr.info(f"get {label} file.")
+        self._lgr.info(label)
         f_dir = HOME_FOLDER
-        if label == FILE_LABEL:
-            f_name, _ = QFileDialog.getOpenFileName(caption = f"Get {label} Files", filter = f"{FILE_LABEL}: All Files (*)",
+        if self.cb_fxn.currentText() == FUNCTIONS[3]:
+            f_name, _ = QFileDialog.getOpenFileName(caption = "Get File", filter = f"{FILE_LABEL}: All Files (*)",
                                                     dir = f_dir, options = QFileDialog.Option.DontUseNativeDialog)
         else:  # folder
-            f_name = QFileDialog.getExistingDirectory(caption = f"Get {label}", dir = f_dir,
+            f_name = QFileDialog.getExistingDirectory(caption = "Get Folder", dir = f_dir,
                                                       options = QFileDialog.Option.DontUseNativeDialog)
 
         if f_name:
             self._lgr.info(f"Selected: {f_name}")
             display_name = get_filename(f_name)
-            self.selected = f_name
-            if label == FILE_LABEL:  # file to send
-                self.file_btn.setText(display_name)
-                self.folder_btn.setText(NO_NEED)
-            else:  # folder to send
-                self.folder_btn.setText(display_name)
-                self.file_btn.setText(NO_NEED)
+            self.f_selected = f_name
+            self.pb_fsend.setText(display_name)
 
-    def fxn_change(self):
-        self._lgr.info(f"fxn_change; current layout = {repr(self.gb_main.layout())}")
-        if self.cb_fxn.currentText() == FUNCTIONS[3]:
-            self.activate_sendfile_options()
-        elif self.cb_fxn.currentText() == FUNCTIONS[2]:
-            self.activate_sendfolder_options()
-        elif self.cb_fxn.currentText() == FUNCTIONS[4]:
-            self.activate_getfilemeta_options()
-        elif self.cb_fxn.currentText() == FUNCTIONS[5]:
-            self.activate_deletefiles_options()
-        elif self.cb_fxn.currentText() == FUNCTIONS[1]:
-            self.activate_getoldfiles_options()
-        elif self.cb_fxn.currentText() == FUNCTIONS[0]:
-            self.activate_getfolders_options()
+    def get_drive_option(self):
+        fct = self.cb_fxn.currentText()
+        if fct == FUNCTIONS[3] or fct == FUNCTIONS[2]:
+            dtitle = "Drive Folder"
+            dlabel = "Enter the name of the Drive folder to search (default = root)"
+        elif fct == FUNCTIONS[4]:
+            dtitle = "Metadata Filename"
+            dlabel = f"Enter the name of the file to query (default = {self.meta_filename})"
         else:
-            raise Exception("?? INVALID Function Choice??!!")
-
-    def get_drive_folder(self):
-        d_folder, ok = QInputDialog.getText(self, "Drive Folder", "Enter the name of the Drive folder to search (default = root)")
+            self._lgr.warning(f"?? INVALID function = '{fct}'")
+            return
+        d_choice, ok = QInputDialog.getText(self, dtitle, dlabel)
         if ok:
-            self.drive_folder = d_folder
-            self._lgr.info(f"Drive folder changed to {d_folder}.")
-            self.pb_drive_folder.setText(d_folder)
+            self._lgr.info(f"Drive option changed to {d_choice}.")
+            if fct == FUNCTIONS[4]:
+                self.meta_filename = d_choice
+            else:
+                self.drive_folder = d_choice
+            self.pb_drive_option.setText(d_choice)
 
-    def get_meta_filename(self):
-        meta, ok = QInputDialog.getText(self, "Metadata Filename",
-                                        f"Enter the name of the file to query (default = {self.meta_filename})")
+    def get_num_files(self):
+        num, ok = QInputDialog.getInt(self, "Number of Files", f"Enter a value (1-{MAX_NUM_ITEMS})",
+                                      value = self.num_files, minValue = 1, maxValue = MAX_NUM_ITEMS)
         if ok:
-            self.meta_filename = meta
-            self._lgr.info(f"Meta file changed to {meta}.")
-            self.pb_meta_filename.setText(meta)
-
-    def mode_change(self):
-        if self.cb_mode.currentText() == TEST_FOLDER:
-            # need Gnucash file and domain only if in SEND mode
-            self.file_btn.setText(NO_NEED)
-            self.selected_file = None
-        else:
-            if self.selected is None:
-                self.folder_btn.setText(self.folder_btn_title)
-
-    def activate_sendfile_options(self):
-        # path to FILE to send
-        # option: drive folder to send to; default = root
-        self._lgr.info("activate_sendfile_options")
-
-    def activate_sendfolder_options(self):
-        # path to FOLDER to send
-        # option: drive folder to send to; default = root
-        self._lgr.info("activate_sendfolder_options")
-
-    def activate_getfilemeta_options(self):
-        # ID OR NAME of file to query
-        self._lgr.info("activate_getfilemeta_options")
-
-    def activate_deletefiles_options(self):
-        # option: drive folder with files to delete; default = TEST_FOLDER
-        # option: type of files to delete; default = DEFAULT_FILETYPE
-        # option: date to delete files OLDER THAN; default = DEFAULT_DATE
-        # option: TEST mode: NO deletions, just report; default = False
-        self._lgr.info("activate_deletefiles_options")
-
-    def activate_getoldfiles_options(self):
-        # ?? ADD? option: drive folder with files to retrieve info; default = root
-        # option: type of files to retrieve info; default = DEFAULT_FILETYPE
-        # option: type of files is a MimeType instead of a filename extension; default = False
-        # option: NUMBER of files to retrieve info on; default = DEFAULT_NUM_FILES, max = MAX_NUM_FILES
-        self._lgr.info("activate_getoldfiles_options")
-
-    def activate_getfolders_options(self):
-        # None
-        self._lgr.info("activate_getfolders_options")
+            self.num_files = num
+            self._lgr.info(f"number of files changed to {num}.")
+            self.pb_logging.setText(f"{NUMFILES_LABEL} -- Current value = {num}")
 
     def get_log_level(self):
-        num, ok = QInputDialog.getInt(self, "Logging Level", "Enter a value (0-100)", value = self.log_level, minValue = 0, maxValue = 100)
+        num, ok = QInputDialog.getInt(self, "Logging Level", "Enter a value (0-100)",
+                                      value = self.log_level, minValue = 0, maxValue = 100)
         if ok:
             self.log_level = num
             self._lgr.info(f"logging level changed to {num}.")
@@ -231,28 +217,28 @@ class DriveFunctionsUI(QDialog):
         """Prepare the parameters string and send to main function of module parseMonarchCopyRep."""
         self._lgr.info(f"Clicked '{self.exe_btn.text()}'.")
 
-        if self.mon_file is None:
+        if self.selected_function is None:
             msg_box = QMessageBox()
             msg_box.setIcon(QMessageBox.Icon.Warning)
             msg_box.setText("MUST select a Monarch Input File!")
             msg_box.exec()
             return
 
-        cl_params = ['-i'+self.mon_file, '-l'+str(self.log_level)]
+        cl_params = ['-i'+self.selected_function, '-l'+str(self.log_level)]
 
         if self.chbx_json.isChecked():
-            cl_params.append('--json')
+            cl_params.append('--jsonsave')
 
-        mode = self.cb_mode.currentText()
+        mode = self.cb_fxn.currentText()
         if mode != TEST_FOLDER:
-            if self.gnc_file is None:
+            if self.f_selected is None:
                 msg_box = QMessageBox()
                 msg_box.setIcon(QMessageBox.Icon.Warning)
                 msg_box.setText("MUST select a Gnucash File!")
                 msg_box.exec()
                 return
             cl_params.append('gnc')
-            cl_params.append('-g'+self.gnc_file)
+            cl_params.append('-g'+self.f_selected)
             cl_params.append('-t'+mode)
 
         self._lgr.info(f"Parameters = \n{json.dumps(cl_params, indent = 4)}\nCalling main_monarch_input...")
